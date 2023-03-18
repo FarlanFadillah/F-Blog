4518c5d98ae3223fb39c8b42d56a93cf35877222
Farlan Fadillah <farlanf61@gmail.com>
2023-03-19 02:28:37 +0700

    Add string representation to models

--- Main/models.py
+++ Main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -9,10 +10,16 @@ class Content(models.Model):
     date = models.DateTimeField(default=datetime.now(), blank=True)
     writer_id = models.IntegerField()
 
+    def __str__(self):
+        return f"on {self.date} : {self.content}"
+
 
 class ProfileImg(models.Model):
     image = models.ImageField(upload_to='images/', default='default-profile.jpg')
     user_id = models.IntegerField()
+
+    def __str__(self):
+        return User.objects.get(id = self.user_id).username
 
 class Comment(models.Model):
     comment = models.CharField(max_length=10000)
@@ -20,7 +27,13 @@ class Comment(models.Model):
     user_id = models.IntegerField()
     date = models.DateTimeField(default=datetime.now(), blank=True)
 
+    def __str__(self):
+        return f"{User.objects.get(id = self.user_id).username}: {self.comment}"
+
 
 class Followed(models.Model):
     my_id = models.IntegerField()
     followed_id = models.IntegerField()
+
+    def __str__(self):
+        return User.objects.get(id = self.my_id).username
